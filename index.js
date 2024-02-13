13c4f381bcf61808fcd14544b3193d084b928cca
Binura Gunasekara <binura.g@platformer.com>
2024-02-14 10:54:24 +1100

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const filterKeysBy = process.env.FILTER_KEY;
+
+const filteredEnv = Object.entries(process.env).filter(([key, value]) => {
+  if (filterKeysBy) {
+    return key.includes(filterKeysBy);
+  }
+  return true;
+});
+
+console.log(filteredEnv);
